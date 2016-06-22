42e4b170f889236b19d3de54826bd7ebd1310aa4
Minseok Lee <lagender@gmail.com>
2016-06-22 16:22:03 +0900

    update usersService, userController

--- src/main/java/com/minseokism/service/UserService.java
+++ src/main/java/com/minseokism/service/UserService.java
@@ -2,121 +2,33 @@ package com.minseokism.service;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.minseokism.domain.User;
 import com.minseokism.repository.UserRepository;
 
-public class UserService implements UserRepository {
-
-	@Override
+@Service
+public class UserService{
+	@Autowired
+	UserRepository userRepository;
 	public List<User> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return userRepository.findAll();
 	}
-
-	@Override
-	public List<User> findAll(Sort sort) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<User> findAll(Iterable<Integer> ids) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends User> List<S> save(Iterable<S> entities) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void flush() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public <S extends User> S saveAndFlush(S entity) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void deleteInBatch(Iterable<User> entities) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void deleteAllInBatch() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public User getOne(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Page<User> findAll(Pageable pageable) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends User> S save(S entity) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
+	
 	public User findOne(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		return userRepository.findOne(id);
 	}
-
-	@Override
-	public boolean exists(Integer id) {
-		// TODO Auto-generated method stub
-		return false;
+	
+	public User create(User user) {
+		return userRepository.save(user);
 	}
-
-	@Override
-	public long count() {
-		// TODO Auto-generated method stub
-		return 0;
+	
+	public User update(User user) {
+		return userRepository.save(user);
 	}
-
-	@Override
+	
 	public void delete(Integer id) {
-		// TODO Auto-generated method stub
-
+		userRepository.delete(id);
 	}
-
-	@Override
-	public void delete(User entity) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void delete(Iterable<? extends User> entities) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void deleteAll() {
-		// TODO Auto-generated method stub
-
-	}
-
 }
--- src/main/java/com/minseokism/web/UserController.java
+++ src/main/java/com/minseokism/web/UserController.java
@@ -1,5 +1,33 @@
 package com.minseokism.web;
 
-public class UserController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.minseokism.domain.User;
+import com.minseokism.service.UserService;
+
+@Controller
+@RequestMapping("users")
+public class UserController {
+	@Autowired
+	UserService userService;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	String List(Model model) {
+		List<User> users = userService.findAll();
+		model.addAttribute("users", users);
+		return "users/list";
+	}
+	
+	@RequestMapping(value = "create", method = RequestMethod.POST)
+	String create(Model model) {
+		User user = new User();
+		userService.create(user);
+		return "redirect:/users";
+	}
 }
