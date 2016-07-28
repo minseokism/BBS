1ddcfc11881f580bbe8f5f0ee4957037ff99aa3f
Minseok Lee <lagender@gmail.com>
2016-07-28 14:11:22 +0900

    password regexp test cases

--- src/test/java/com/minseokism/service/UserServiceTest.java
+++ src/test/java/com/minseokism/service/UserServiceTest.java
@@ -1,6 +1,9 @@
 package com.minseokism.service;
 
-import org.junit.After;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,17 +26,38 @@ public class UserServiceTest {
 	User user;
 	
 	@Test
-	public void signupValidateTest() {	
-		user = new User();
-		user.setEmail("ediya@naver.com");
-		user.setId("ediya_us");
-		user.setName("이디야");
-		user.setPwd("dleldi12");
-		userRepository.save(user);
+	public void pwdRegexpTest() {
+		HashMap<String, Boolean> testCase = new HashMap<String, Boolean>();
+		testCase.put("12345678", true);
+		testCase.put("abcdefghi", true);
+		testCase.put("ABSDSACAEAKDSK", true);
+		testCase.put("abcdefghi0@", true);
+		testCase.put("@abcdefghi0", true);
+		testCase.put("Xod%#%#a9921(5)", true);
+		testCase.put("!@#$%^&*_()+-={}[]~`", true);
+		testCase.put(",.,.,.,.?/;:'\"", true);
+		testCase.put("<script>", true);
+		testCase.put("", false);
+		testCase.put("abcde", false);
+		
+		String regexp ="[A-Za-z0-9!@#$%^&*'\"<>,;:./?_\\(\\)\\{\\}\\[\\]~`+=-]{6,20}$";
+		
+		for(Entry<String, Boolean> entry : testCase.entrySet()){
+			Assert.assertEquals(entry.getValue(), entry.getKey().matches(regexp));
+		}
+
 	}
 	
-	@After
-	public void delUser(){
+	@Test
+	public void signupValidateTest() {	
+		user = new User();
+		user.setEmail("ediya2@naver.com");
+		user.setId("ediya_us2");
+		user.setName("이디야");
+		user.setPwd("Asdsadsad");
+		
+		userRepository.save(user);
 		userRepository.delete(user);
 	}
+	
 }
