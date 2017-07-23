9270f6b8b5567310f8caad9d9da98349fb9ece4b
Minseok Lee <lagender@gmail.com>
2017-07-23 22:32:09 +0900

    Changed domain

--- src/main/java/com/minseokism/domain/User.java
+++ src/main/java/com/minseokism/domain/User.java
@@ -28,31 +28,82 @@ public class User {
 	@GeneratedValue
 	@Column(name = "user_no")
 	private Integer no;
-	
+
 	@Pattern(regexp = "^[a-z0-9][a-z0-9_-]{4,29}$")
 	@Column(nullable = false)
 	private String id;
 
 	@Column(nullable = false)
 	private String pwd;
-	
+
 	@Pattern(regexp = "^[a-zA-Z0-9_-[+]]+@[a-zA-Z0-9//.]+$")
 	@Column(nullable = false)
 	private String email;
-	
+
 	@Pattern(regexp = "^[가-힣ㅣa-zA-Z0-9]{0,19}$")
 	@Column(nullable = false)
 	private String name;
-	
+
+	private String token;
+
 	@Transient
 	private int state;
+
+	public Integer getNo() {
+		return no;
+	}
+
+	public void setNo(Integer no) {
+		this.no = no;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public int getState() {
+		return state;
+	}
+
+	public void setState(int state) {
+		this.state = state;
+	}
 	
-	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-	private List<Token> tokens = new ArrayList<Token>();
 	
-	public boolean addToken(Token token) {
-		if(tokens == null)
-			tokens = new ArrayList<>(); 
-		return this.tokens.add(token);
-    }
 }
